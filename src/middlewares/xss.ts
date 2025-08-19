e3e1241d7f80da8ae92da6ab34b6779042094696
Eskinder Abera <eske17.ea@gmail.com>
2025-08-19 15:27:50 +0300

    add xss security

--- src/middlewares/xss.ts
+++ src/middlewares/xss.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import xss from "xss";
 
+// Sanitize a single value
 const clean = (data: any) => {
   if (typeof data === "object") {
     return JSON.parse(xss(JSON.stringify(data)));
@@ -8,11 +9,24 @@ const clean = (data: any) => {
   return xss(String(data));
 };
 
+// Sanitize all keys in an object
+const sanitizeObject = (obj: Record<string, any>) => {
+  for (const key of Object.keys(obj)) {
+    obj[key] = clean(obj[key]);
+  }
+};
+
 const middleware = () => {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (req.body) req.body = clean(req.body);
-    if (req.query) req.query = clean(req.query);
-    if (req.params) req.params = clean(req.params);
+    if (req.body && typeof req.body === "object") {
+      sanitizeObject(req.body);
+    }
+    if (req.query && typeof req.query === "object") {
+      sanitizeObject(req.query);
+    }
+    if (req.params && typeof req.params === "object") {
+      sanitizeObject(req.params);
+    }
     next();
   };
 };
